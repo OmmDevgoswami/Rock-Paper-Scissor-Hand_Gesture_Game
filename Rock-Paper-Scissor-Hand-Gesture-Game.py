c5599ce550d7de6200327c988a801d2543e99b9c
Omm Devgoswami <127383096+OmmDevgoswami@users.noreply.github.com>
2024-07-04 22:29:59 +0530

    Add files via upload
    Main File Added.

---
+++ Rock-Paper-Scissor-Hand-Gesture-Game.py
@@ -0,0 +1,102 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import random
+
+#ASCII ART
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print ("Welcome to the Rock-Paper-Scissor Python Game !!")
+print ("Special Hand Gesture Version !!")
+start = input ("Press 'Y' to Play or 'N' to Exit: ")
+x = 1
+player = 0
+
+if start == "Y":
+    cam = cv2.VideoCapture(0)
+    detector = HandDetector(maxHands = 1)
+    while x == 1:
+        success, img = cam.read()
+        hands, img = detector.findHands(img, draw = True)
+    
+        cv2.imshow("Images",img)
+        if cv2.waitKey(1) & 0xFF == ord("0"): #Press 0 to quite
+            x = 0
+    
+        if hands:
+            hand = hands[0]
+            fingers = detector.fingersUp(hand)
+            if fingers == [0, 0, 0, 0, 0] or fingers == [1, 0, 0, 0, 0]:
+                player = 1
+                x = 0
+            elif fingers == [1, 1, 1, 1, 1] or fingers == [0, 1, 1, 1, 1]:
+                player = 2
+                x = 0
+            elif fingers == [0, 1, 1, 0, 0] or fingers == [1, 1, 1, 0, 0]:
+                player = 3
+                x = 0
+            else:
+                print ("Invalid Input.")
+
+
+computer = random.randint(1,3)
+if (player == 1):
+    print ("Player Chose: Rock"+rock)
+    if (computer == 1):
+        print ("Computer Chose: Rock"+rock)
+        print ("This is a Tie !!")
+    elif (computer == 2):
+        print ("Computer Chose: Paper"+paper)
+        print ("You Lose !!")
+    else:
+        print ("Computer Chose: Scissor"+scissors)
+        print ("You Win !!")
+elif (player == 2):
+    print ("Player Chose: Paper"+paper)
+    if (computer == 2):
+        print ("Computer Chose: Paper"+paper)
+        print ("This is a Tie !!")
+    elif (computer == 3):
+        print ("Computer Chose: Scissor"+scissors)
+        print ("You Lose !!")
+    else:
+        print ("Computer Chose: Rock"+rock)
+        print ("You Win !!")
+elif (player == 3):
+    print ("Player Chose: Scissor"+scissors)
+    if (computer == 3):
+        print ("Computer Chose: Scissor"+scissors)
+        print ("This is a Tie !!")
+    elif (computer == 1):
+        print ("Computer Chose: Rock"+rock)
+        print ("You Lose !!")
+    else:
+        print ("Computer Chose: Paper"+paper)
+        print ("You Win !!")
+else:
+    print ("Game Exited !! Thanks for Playing")
+        
